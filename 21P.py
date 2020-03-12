f7881e96448b97f7240656cff7ddc4a5860c0cd2
Helio Giroto <31024350+HelioGiroto@users.noreply.github.com>
2020-03-12 18:12:55 -0300

    Update 21P.py

--- 21P.py
+++ 21P.py
@@ -2,7 +2,9 @@
 line = '21P/Giacobini-Zinner,e,32.0059,195.3730,172.8480,3.496349,0.1507585,0.71054100,82.7184,03/12.0/2020,2000,g  9.0,6.0'
 
 yh = ephem.readdb(line)
- 
+
+print(yh)
+
 yh.compute('2020/03/12')
  
 print(yh.earth_distance)
