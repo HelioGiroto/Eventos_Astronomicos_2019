e1f052db74ec45b081fd5c99cc81c7da20156b53
Helio Giroto <31024350+HelioGiroto@users.noreply.github.com>
2020-03-12 18:07:57 -0300

    Update 21P.py

--- 21P.py
+++ 21P.py
@@ -1,6 +1,6 @@
 
 line = '21P/Giacobini-Zinner,e,32.0059,195.3730,172.8480,3.496349,0.1507585,0.71054100,82.7184,03/12.0/2020,2000,g  9.0,6.0'
- 
+
 yh = ephem.readdb(line)
  
 yh.compute('20200312')
@@ -17,4 +17,6 @@ print(yh.mag)
 76.21
  
 
+# fonts: 
+# https://minorplanetcenter.net/iau/Ephemerides/Comets/Soft03Cmt.txt
 
