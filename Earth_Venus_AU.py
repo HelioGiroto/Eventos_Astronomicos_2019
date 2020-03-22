c41eddeb00dd4e614f274eaac6dce1c2308fc74e
Helio Giroto <31024350+HelioGiroto@users.noreply.github.com>
2020-03-21 21:06:50 -0300

    Distancia VENUS - TERRA 2020

---
+++ Earth_Venus_AU.py
@@ -0,0 +1,12 @@
+import ephem
+venus = ephem.Venus()
+dia = ephem.Date('2020/01/01')
+
+for i in range(366):
+   date = ephem.Date(dia)
+   au = ephem.Venus(dia).earth_distance
+   print(str(date) + ' = ' + str(au))
+   dia += 1
+ 
+# Este script usando EPHEM não é tão preciso na distancia como o skyfield!!
+# Ver script mais preciso em: 
